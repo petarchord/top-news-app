c41f24fcc4f42620edaab8586bb2b74c76063d26
petarchord <petarchord@hotmail.com>
2020-11-25 16:57:27 +0100

    category sliding done

--- top-news-app/src/components/CategoryType/CategoryType.jsx
+++ top-news-app/src/components/CategoryType/CategoryType.jsx
@@ -6,6 +6,18 @@ import { Link } from "react-router-dom";
 
 const CategoryType = ({ category, country }) => {
   const [news, setNews] = useState([]);
+  const [categoryIndex, setCategoryIndex] = useState(0);
+
+  const slideLeft = () => {
+    if (categoryIndex === 0) setCategoryIndex(news.length - 1);
+    else setCategoryIndex((prevIndex) => prevIndex - 1);
+  };
+
+  const slideRight = () => {
+    if (categoryIndex === news.length - 1) setCategoryIndex(0);
+    else setCategoryIndex((prevIndex) => prevIndex + 1);
+  };
+
   const fetchCategory = async () => {
     setNews(await fetchNewsByCategory(country, category.toLowerCase(), 5));
   };
@@ -18,13 +30,25 @@ const CategoryType = ({ category, country }) => {
     <div className={styles.container}>
       <h3>{category}</h3>
       <div className={styles.content_holder}>
-        <BsChevronLeft size="3em" color="white" />
-        <BsChevronRight size="3em" color="white" />
-        <h5> {news.length && news[0].title} </h5>
+        <BsChevronLeft
+          size="3em"
+          color="white"
+          onClick={() => {
+            slideLeft();
+          }}
+        />
+        <BsChevronRight
+          size="3em"
+          color="white"
+          onClick={() => {
+            slideRight();
+          }}
+        />
+        <h5> {news.length && news[categoryIndex].title} </h5>
         <Link to={`/categories/${category.toLowerCase()}`}>
           <img
-            src={news.length && news[0].urlToImage}
-            alt={news.length && news[0].title + "_img"}
+            src={news.length && news[categoryIndex].urlToImage}
+            alt={news.length && news[categoryIndex].title + "_img"}
           />
         </Link>
       </div>
